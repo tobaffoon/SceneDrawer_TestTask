851d34186cac2c72a8493bba7de6452c9613041e
Alexei Maziar <a.mathjar@gmail.com>
2024-10-22 17:59:41 +0700

    Hide Line properties in VLine and HLine
    To make sure that they preserve horizontality/verticality

--- SceneDrawer/SceneObjects/HLine.cs
+++ SceneDrawer/SceneObjects/HLine.cs
@@ -1,5 +1,22 @@
 ﻿namespace SceneDrawer.SceneObjects {
 	public abstract class HLine : Line {
+		private int _y;
+		public new int Y1 {
+			get {
+				return _y;
+			}
+			set {
+				_y = value;
+			}
+		}
+		public new int Y2 {
+			get {
+				return _y;
+			}
+			set {
+				_y = value;
+			}
+		}
 		public HLine(int x1, int x2, int y) : base(x1, y, x2, y) { }
 	}
 }
--- SceneDrawer/SceneObjects/VLine.cs
+++ SceneDrawer/SceneObjects/VLine.cs
@@ -1,5 +1,23 @@
 ﻿namespace SceneDrawer.SceneObjects {
 	public abstract class VLine : Line {
+		private int _x;
+		public new int X1 {
+			get {
+				return _x;
+			}
+			set {
+				_x = value;
+			}
+		}
+		public new int X2 {
+			get {
+				return _x;
+			}
+			set {
+				_x = value;
+			}
+		}
+
 		public VLine(int x, int y1, int y2) : base(x, y1, x, y2) { }
 	}
 }
