60f76e089021091a5af4e36212bd43286a8a6198
Alexei Maziar <a.mathjar@gmail.com>
2024-10-22 12:42:03 +0700

    Add simple arg parsing for input file

--- SceneDrawerExample/Program.cs
+++ SceneDrawerExample/Program.cs
@@ -5,7 +5,14 @@ namespace SceneDrawerExample {
 	public class Program() {
 		public static void Main(string[] args) {
 			SceneTextFileParser sceneParser = new SceneTextFileParser();
-            string path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Properties.Resources.InputFilePath);
+
+			string path;
+			if (args.Length > 0 && File.Exists(args[0])) {
+				path = args[0];
+			}
+			else {
+				path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Properties.Resources.InputFilePath);
+			}
 
 			Scene scene = sceneParser.ParseScene(path);
 			foreach (DrawObject dobj in scene.DrawObjects) {
