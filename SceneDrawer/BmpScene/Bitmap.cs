5ab69e0a2aa15438464a02a38ed3bfb29777d7dd
Alexei Maziar <a.mathjar@gmail.com>
2024-10-22 14:58:22 +0700

    Convert bitmap class to interface

--- SceneDrawer/BmpScene/Bitmap.cs
+++ SceneDrawer/BmpScene/Bitmap.cs
@@ -1,21 +1,10 @@
 ﻿namespace SceneDrawer.BmpScene {
-	public class Bitmap {
+	public interface IBitmap {
 		public int Width { get; }
 		public int Height { get; }
-		public readonly bool[,] pixels;
 
-		public Bitmap(int width, int height) {
-			Width = width;
-			Height = height;
-			pixels = new bool[Width, Height];
-		}
+		public void SetPixel(int x, int y, byte[] value);
 
-		public void SetPixel(int x, int y, bool value = true) {
-			pixels[x, y] = value;
-		}
-
-		public bool GetPixel(int x, int y) {
-			return pixels[x, y];
-		}
+		public byte[] GetPixel(int x, int y);
 	}
 }
---
+++ SceneDrawer/BmpScene/BitmapDrawer.cs
@@ -0,0 +1,6 @@
+﻿namespace SceneDrawer.BmpScene {
+	public class BitmapDrawer {
+		public Bitmap Bitmap { get; }
+		public int UserSceneX1 
+	}
+}
