c8bb99233f2d5d4b9aeaa7252b3553a001d8ba5f
Alexei Maziar <a.mathjar@gmail.com>
2024-10-22 13:08:16 +0700

    Create bmpFileDrawer folder

--- SceneDrawer/BmpFileDrawer/SceneBmpFileDrawer.cs
+++ SceneDrawer/BmpFileDrawer/SceneBmpFileDrawer.cs
@@ -1,6 +1,7 @@
 ﻿
-namespace SceneDrawer {
+namespace SceneDrawer.BmpFileDrawer {
 	public class SceneBmpFileDrawer : ISceneDrawer {
+		public readonly byte[] BlackPixel = [0x00, 0x00, 0x00];
 		public void DrawScene(Scene scene, Stream oStream) {
 			throw new NotImplementedException();
 		}
