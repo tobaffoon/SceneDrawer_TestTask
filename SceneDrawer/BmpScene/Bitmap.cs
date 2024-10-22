5c46c560e03180341bd646337d07614d6f587025
Alexei Maziar <a.mathjar@gmail.com>
2024-10-22 14:47:12 +0700

    Add Bitmap class

---
+++ SceneDrawer/BmpScene/Bitmap.cs
@@ -0,0 +1,21 @@
+﻿namespace SceneDrawer.BmpScene {
+	public class Bitmap {
+		public int Width { get; }
+		public int Height { get; }
+		public readonly bool[,] pixels;
+
+		public Bitmap(int width, int height) {
+			Width = width;
+			Height = height;
+			pixels = new bool[Width, Height];
+		}
+
+		public void SetPixel(int x, int y, bool value = true) {
+			pixels[x, y] = value;
+		}
+
+		public bool GetPixel(int x, int y) {
+			return pixels[x, y];
+		}
+	}
+}
